2da33c3f34cb9a5bbe15d26a5c38fa9bfa022eeb
Avraham Shor <avraham.shor@ravtech.co.il>
2023-05-21 18:10:47 +0300

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
+
 let gemOfTorah = tora_0_300;
 
-
+console.log(JSON.stringify(objTora, null, 4));
 // console.log(gemObject);
 // gemObject.forEach(n => {
 //     console.log(n);
@@ -37,12 +39,13 @@ let gemOfTorah = tora_0_300;
 function setGematrya(value) {
     let history = [];
     const sum = calculate(value);
+    switchObject(sum);
     document.getElementById('sum').innerText = sum;
     localStorage.getItem('history') ? history = localStorage.getItem('history').split('%%') : [];
     history = history.filter((v, i) => history.indexOf(v) === i).sort();
     document.getElementById('history').innerText = history.filter(e => calculate(e) == sum && e != value);
     if (document.getElementById('history').innerText.length < 1) {
-        document.getElementById('history').innerText = ' אין מילים שמתאימות לגימטריא זו';
+        document.getElementById('history').innerText = ' אין מילים בהיסטוריה שלך שמתאימות לגימטריא זו';
     }
     let cleanValue = clean(value);
     if (sum > 0 && value.length > 1) history.push(cleanValue + " ");
@@ -54,32 +57,6 @@ function setGematrya(value) {
 
 }
 
-function setListOfNumbersInTora(gemOfTorah, perek) {
-    if (perek.he) {
-        perek.he.forEach((e, index) => {
-            const sourcePasuk = addIndexPasuk(index + 1);
-            wordsOfPasuk = e.split(' ');
-            for (let i = 0; i < wordsOfPasuk.length; i++) {
-                for (let j = i + 1; j <= wordsOfPasuk.length; j++) {
-                    let words = wordsOfPasuk.slice(i, j);
-                    if (words.length) {
-                        let word = words.join(' ');
-                        word = clean(word);
-                        let dict = {};
-                        dict[word] = perek.heRef + sourcePasuk; 
-                        if (!gemOfTorah[calculate(word)]) {
-                            gemOfTorah[calculate(word)] = [];
-                        }
-                        // console.log('word:', word, rejects(word) );
-                        wordWithoutNikud = rejects(word);
-                        gemOfTorah[calculate(word)] = gemOfTorah[calculate(word)].filter(w => rejects(Object.keys(w)[0]) != wordWithoutNikud);
-                        gemOfTorah[calculate(word)].push(dict);
-                    }
-                }
-            }
-        });
-    }
-}
 
 function addIndexPasuk(index) {
     const some = index % 10;
@@ -88,39 +65,46 @@ function addIndexPasuk(index) {
     console.log(hundreds, tens, some);
     console.log(VAL_OBVERSE[hundreds] + VAL_OBVERSE[tens] + VAL_OBVERSE[some]);
     return ' ' + VAL_OBVERSE[hundreds] + VAL_OBVERSE[tens] + VAL_OBVERSE[some];
-    
+
 }
 
 function addToTheTable(sum) {
     torahValues = gemOfTorah[sum];
-    let tableRef = document.getElementById("table");
+    let tableRef = document.getElementById("the-value");
     while (tableRef.rows.length > 1) {
         tableRef.deleteRow(1);
     }
     if (torahValues && torahValues.length) {
         torahValues.forEach((arr) => {
-            console.log(arr);
-           
-            const pasuk = arr[0] + arr[1]  +arr[2];
-            const value = arr[3];
+
+            const mainOfPasuk = arr[1];
+
+            const wordSpan = document.createElement("span");
+            wordSpan.style.backgroundColor = "yellow";
+            wordSpan.textContent = mainOfPasuk;
+
+            const source = arr[3];
+
+
 
             let newRow = tableRef.insertRow(-1);
 
             let sourceCell = newRow.insertCell(0);
-            let wordCell = newRow.insertCell(1);
+            let pasukCell = newRow.insertCell(1);
 
-
-            let sourceText = document.createTextNode(pasuk);
-            let wordText = document.createTextNode(value);
+            let sourceText = document.createTextNode(source);
+            let pasukText1 = document.createTextNode(arr[0]);
+            let pasukText2 = document.createTextNode(arr[2]);
 
             sourceCell.appendChild(sourceText);
-            wordCell.appendChild(wordText);
-
+            pasukCell.appendChild(pasukText1);
+            pasukCell.appendChild(wordSpan);
+            pasukCell.appendChild(pasukText2);
 
             //from chat GPT
-            // let wordSpan = document.createElement("span"); // Create a <span> element
-            // wordSpan.style.backgroundColor = "yellow"; // Set yellow background color
-            // wordSpan.textContent = value; // Set the text content of the <span>
+            //  // Create a <span> element
+            //  // Set yellow background color
+            //  value; // Set the text content of the <span>
 
             // sourceCell.appendChild(sourceText);
             // wordCell.appendChild(wordSpan); // Append the <span> element to the cell
@@ -153,4 +137,94 @@ function clean(word) {
 
         return word.replace(/[a-z]|[0-9]|<|>|-|"|=|/g, "").replace("{ס}", "").replace("{פ}", "").replace("/", "").replace("|", "");
     }
+}
+
+function setListOfNumbersInTora(obj) {
+    if (perek.he) {
+        console.log(perek.he.length, min);
+        perekLength = perek.he.length * 22;
+        perek.he.forEach((pasuk, index) => {
+            // console.log(e, index);
+            // console.log(1, pasuk);
+            pasuk = clean(pasuk);
+            // console.log(2, pasuk);
+            const sourcePasuk = addIndexPasuk(index + 1);
+            wordsOfPasuk = pasuk.split(' ');
+            for (let i = 0; i < wordsOfPasuk.length; i++) {
+                for (let j = i + 1; j <= wordsOfPasuk.length; j++) {
+                    let words = wordsOfPasuk.slice(i, j);
+                    if (words.length) {
+                        let word = words.join(' ');
+                        if (inRange(calculate(word), min)) {
+                            let entirePasuk = pasuk.split(word);
+                            const pasukWithSource = [entirePasuk[0], word, entirePasuk[1], perek.heRef + sourcePasuk];
+                            if (!gemOfTorah[calculate(word)]) {
+                                gemOfTorah[calculate(word)] = [];
+                            }
+                            // console.log('word:', word, rejects(word) );
+                            wordWithoutNikud = rejects(word);
+                            //gemOfTorah[calculate(word)] = ;
+                            if (!gemOfTorah[calculate(word)].filter(pasuk => rejects(pasuk[1]) == wordWithoutNikud).length) {
+                                gemOfTorah[calculate(word)].push(pasukWithSource);
+                            }
+
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+}
+
+function switchObject(sum) {
+    switch (inRange(sum)) {
+        case 0:
+            gemOfTorah = tora_0_300;
+            break;
+        case 300:
+            gemOfTorah = tora_300_600;
+            break;
+        case 600:
+            gemOfTorah = tora_600_900;
+            break;
+        case 900:
+            gemOfTorah = tora_900_1200;
+            break;
+        case 1200:
+            gemOfTorah = tora_1200_1500;
+            break;
+        case 1500:
+            gemOfTorah = tora_1500_1800;
+            break;
+        case 1800:
+            gemOfTorah = tora_1800_2100;
+            break;
+        case 2100:
+            gemOfTorah = tora_2100_2400;
+            break;
+        case 2400:
+            gemOfTorah = tora_2400_2700;
+            break;
+        case 2700:
+            gemOfTorah = tora_2700_3000;
+            break;
+        case 3000:
+            gemOfTorah = tora_3000_3600;
+            break;
+
+
+        default:
+            break;
+    }
+}
+
+function inRange(sum) {
+    if (sum < 3000) {
+        return sum - sum % 300;
+    }
+    if (sum >= 6000) {
+        return end
+    }
+    return sum - sum % 600;
 }
